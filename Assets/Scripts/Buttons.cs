c24f46b814cffd560c9d90d4d74ffa69cacbeb07
Jonathan Augustin <170984340+GameDev47631@users.noreply.github.com>
2025-05-15 15:33:09 -0400

    Credits also in progress.

--- Assets/Scripts/Buttons.cs
+++ Assets/Scripts/Buttons.cs
@@ -1,18 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class Buttons : MonoBehaviour
-{
+public class Buttons : MonoBehaviour {
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start() {
+        //
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+        //
+    }
+
+    public void OnMouseDown() {
+        switch (gameObject.name) {
+            case "CreditsButton":
+                SceneManager.LoadScene("Credits");
+                break;
+            
+            default:
+                Debug.LogWarning("Button ID name changed!");
+                break;
+        }
     }
 }
