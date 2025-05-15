6ea6da49b6e84715d16c7a8f7523f78289da7c3a
Jonathan Augustin <170984340+GameDev47631@users.noreply.github.com>
2025-05-14 22:45:34 -0400

    Added a script to use the animations.

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour {
+    /* https://www.youtube.com/watch?v=CE9VOZivb3I?t=556*/
+    
+    public Animator transition;
+    public float transitioning = 1f;
+
+    // Update is called once per frame
+    void Update() {
+        // "Clicking the button."
+        if (Input.GetMouseButtonDown(0)) {
+            LoadNextLevel();
+        }
+    }
+
+    public void LoadNextLevel() {
+        StartCoroutine(LoadLevel((SceneManager.GetActiveScene().buildIndex + 1)));
+    }
+
+    IEnumerator LoadLevel(int levelIndex) {
+        // "Animate the scene transition."
+        transition.SetTrigger("Start");
+
+        // "Wait for the animation to finish completely."
+        yield return new WaitForSeconds(transitioning);
+
+        // "Now, you can interact with the next scene."
+        SceneManager.LoadScene(levelIndex);
+    }
+}
