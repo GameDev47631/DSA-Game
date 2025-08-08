5b8cfc22b1b69a059e5631958e82a3e7da819a56
Jonathan Augustin <170984340+GameDev47631@users.noreply.github.com>
2025-08-07 21:01:43 -0400

    Crosshairs now have a 'selection' format.

--- Assets/Scripts/DijkstraGameplay.cs
+++ Assets/Scripts/DijkstraGameplay.cs
@@ -3,13 +3,52 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class DijkstraGameplay : MonoBehaviour {
+
+    [SerializeField] GameObject[] Crosshairs;
+    private int currentNode = 0;
+
     // Start is called before the first frame update
     void Start() {
-        
+        // "The number of crosshairs will depend on how many nodes there are."
+        if (Crosshairs.Length > 0) {
+            for (int i = 0; i < Crosshairs.Length; i++) {
+                Crosshairs[i].SetActive(i == currentNode);
+            }
+        }
     }
 
     // Update is called once per frame
     void Update() {
-        
+        // "Use the arrow keys to select the current node."
+        if (Input.GetKeyDown(KeyCode.LeftArrow)) {
+            Selection(-1);
+        } else if (Input.GetKeyDown(KeyCode.RightArrow)) {
+            Selection(1);
+        }
+    }
+
+    void Selection(int direction) {
+        if (Crosshairs.Length == 0) {
+            return;
+        }
+
+        Crosshairs[currentNode].SetActive(false);
+
+        currentNode += direction;
+        if (currentNode < 0) {
+            currentNode = Crosshairs.Length - 1;
+        } else if (currentNode >= Crosshairs.Length) {
+            currentNode = 0;
+        }
+
+        Crosshairs[currentNode].SetActive(true);
+    }
+
+    void ToggleActiveState(bool active) {
+        if (Crosshairs.Length == 0) {
+            return;
+        }
+    
+        Crosshairs[currentNode].SetActive(active);
     }
 }
