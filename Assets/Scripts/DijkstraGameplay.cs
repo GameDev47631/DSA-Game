2c221c3404c158c1bafe462e42ede62c83851067
Jonathan Augustin <170984340+GameDev47631@users.noreply.github.com>
2025-08-08 16:45:07 -0400

    Selection no longer wraps around.

--- Assets/Scripts/DijkstraGameplay.cs
+++ Assets/Scripts/DijkstraGameplay.cs
@@ -19,18 +19,19 @@ public class DijkstraGameplay : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
-        // "Use the arrow keys to select the current node."
+        /* "Use the arrow keys to select the current node." */
         if (Input.GetKeyDown(KeyCode.LeftArrow)) {
             Selection(-1);
         } else if (Input.GetKeyDown(KeyCode.RightArrow)) {
             Selection(1);
         }
 
-        if (Input.GetKeyDown(KeyCode.UpArrow)) {
-            ToggleActiveState(true);
-        } else if (Input.GetKeyDown(KeyCode.DownArrow)) {
-            ToggleActiveState(false);
-        }
+        // if (Input.GetKeyDown(KeyCode.UpArrow)) {
+        //     ToggleActiveState(true);
+        // } else if (Input.GetKeyDown(KeyCode.DownArrow)) {
+        //     ToggleActiveState(false);
+        // }
+
     }
 
     void Selection(int direction) {
@@ -38,23 +39,31 @@ public class DijkstraGameplay : MonoBehaviour {
             return;
         }
 
-        Crosshairs[currentNode].SetActive(false);
+        /* "This code a wrap around selection." */
+        // currentNode += direction;
+        // if (currentNode < 0) {
+        //     currentNode = Crosshairs.Length - 1;
+        // } else if (currentNode >= Crosshairs.Length) {
+        //     currentNode = 0;
+        // }
 
-        currentNode += direction;
-        if (currentNode < 0) {
-            currentNode = Crosshairs.Length - 1;
-        } else if (currentNode >= Crosshairs.Length) {
-            currentNode = 0;
+        int nextNode = currentNode + direction;
+
+        /* "Node selections no longer wrap around."*/
+        if (nextNode < 0 || nextNode >= Crosshairs.Length) {
+            return;
         }
 
+        Crosshairs[currentNode].SetActive(false);
+        currentNode = nextNode;
         Crosshairs[currentNode].SetActive(true);
     }
 
-    void ToggleActiveState(bool active) {
-        if (Crosshairs.Length == 0) {
-            return;
-        }
-    
-        Crosshairs[currentNode].SetActive(active);
-    }
+    // void ToggleActiveState(bool active) {
+    //     if (Crosshairs.Length == 0) {
+    //         return;
+    //     }
+    //
+    //     Crosshairs[currentNode].SetActive(active);
+    // }
 }
